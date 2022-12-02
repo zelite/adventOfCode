98ef773a89eb92eba61c3899acab345fa2d2cf7c
Zelite <jcafernandes@gmail.com>
2022-12-02 21:20:35 +0100

    day 2

---
+++ src/main/kotlin/day2/part1/day2Part1.kt
@@ -0,0 +1,82 @@
+package day2.part1
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+fun main() {
+    val allLines = Files.readAllLines(Path.of("src/main/kotlin/day2/input.txt"))
+    val totalScore = allLines.map { s -> s.split(" ") }
+        .map { l -> Pair(translateElfMoves(l[0]), translatePlayerMoves(l[1])) }
+        .map { p ->
+            scoreForResult(
+                determineWinner(
+                    p.first,
+                    p.second
+                )
+            ) + scoreForPlayerMove(p.second)
+        }.sum()
+    println("Total Score: " + totalScore)
+}
+
+fun scoreForResult(winner: String?): Int {
+    return when (winner) {
+        "Elf" -> 0
+        "Draw" -> 3
+        else -> 6
+    }
+}
+
+fun translateElfMoves(move: String): String {
+    return when (move) {
+        "A" -> "Rock"
+        "B" -> "Paper"
+        else -> "Scissors"
+    }
+}
+
+fun translatePlayerMoves(move: String): String {
+    return when (move) {
+        "X" -> "Rock"
+        "Y" -> "Paper"
+        else -> "Scissors"
+    }
+}
+
+fun scoreForPlayerMove(move: String): Int {
+    return when (move) {
+        "Rock" -> 1
+        "Paper" -> 2
+        else -> 3
+    }
+}
+
+
+fun determineWinner(elfMove: String, playerMove: String): String? {
+    val resultsMap = mapOf(
+        Pair(
+            "Rock",
+            mapOf(
+                Pair("Rock", "Draw"),
+                Pair("Paper", "Player"),
+                Pair("Scissors", "Elf")
+            )
+        ),
+        Pair(
+            "Paper",
+            mapOf(
+                Pair("Rock", "Elf"),
+                Pair("Paper", "Draw"),
+                Pair("Scissors", "Player")
+            )
+        ),
+        Pair(
+            "Scissors",
+            mapOf(
+                Pair("Rock", "Player"),
+                Pair("Paper", "Elf"),
+                Pair("Scissors", "Draw")
+            )
+        )
+    )
+    return resultsMap.get(elfMove)?.get(playerMove)
+}
---
+++ src/main/kotlin/day2/part2/day2Part2.kt
@@ -0,0 +1,77 @@
+package day2.part2
+
+import java.nio.file.Files
+import java.nio.file.Path
+
+fun main() {
+    val allLines = Files.readAllLines(Path.of("src/main/kotlin/day2/input.txt"))
+    val totalScore = allLines.map { s -> s.split(" ") }
+        .map { l -> Pair(translateElfMoves(l[0]), translateResult(l[1])) }
+        .map { p ->
+            scoreForPlayerMove(determinePlayerMove(p.first, p.second)) + scoreForResult(p.second)
+        }.sum()
+    println("Total Score: " + totalScore)
+}
+
+fun scoreForResult(winner: String?): Int {
+    return when (winner) {
+        "Elf" -> 0
+        "Draw" -> 3
+        else -> 6
+    }
+}
+
+fun translateElfMoves(move: String): String {
+    return when (move) {
+        "A" -> "Rock"
+        "B" -> "Paper"
+        else -> "Scissors"
+    }
+}
+
+fun translateResult(move: String): String {
+    return when (move) {
+        "X" -> "Elf"
+        "Y" -> "Draw"
+        else -> "Player"
+    }
+}
+
+fun scoreForPlayerMove(move: String?): Int {
+    return when (move) {
+        "Rock" -> 1
+        "Paper" -> 2
+        else -> 3
+    }
+}
+
+
+fun determinePlayerMove(elfMove: String, playerMove: String): String? {
+    val resultsMap = mapOf(
+        Pair(
+            "Rock",
+            mapOf(
+                Pair("Draw", "Rock"),
+                Pair("Player", "Paper"),
+                Pair("Elf", "Scissors")
+            )
+        ),
+        Pair(
+            "Paper",
+            mapOf(
+                Pair("Elf", "Rock"),
+                Pair("Draw", "Paper"),
+                Pair("Player", "Scissors")
+            )
+        ),
+        Pair(
+            "Scissors",
+            mapOf(
+                Pair("Player", "Rock"),
+                Pair("Elf", "Paper"),
+                Pair("Draw", "Scissors")
+            )
+        )
+    )
+    return resultsMap.get(elfMove)?.get(playerMove)
+}
