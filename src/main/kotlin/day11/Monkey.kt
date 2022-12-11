93587d0356d41378c95bbb190fdedb7e7963abef
Zelite <jcafernandes@gmail.com>
2022-12-11 16:59:39 +0100

    day 11 - part 2

--- src/main/kotlin/day11/Monkey.kt
+++ src/main/kotlin/day11/Monkey.kt
@@ -1,47 +1,49 @@
 package day11
 
 class Monkey(monkeyDescription: List<String>) {
-    private var divisibleBy: Int
-    val falseTarget: Int
-    val trueTarget: Int
-    val divisibleTest: (Int) -> Boolean
-    val holdingItems = ArrayDeque<Int>()
-    val operation: (Int) -> Int
+    var reduceWorryFactor: Long = 0
+    var divisibleBy: Long
+    private val falseTarget: Int
+    private val trueTarget: Int
+    val divisibleTest: (Long) -> Boolean
+    private val holdingItems = ArrayDeque<Long>()
+    val operation: (Long) -> Long
     var inspectionCount = 0L
 
     init {
-        monkeyDescription[1].drop("  Starting items: ".length).split(", ").map { it.toInt() }
+        monkeyDescription[1].drop("  Starting items: ".length).split(", ").map { it.toLong() }
             .toCollection(holdingItems)
 
         operation = parseOperation(monkeyDescription[2])
-        divisibleBy = monkeyDescription[3].drop("  Test: divisible by ".length).toInt()
-        divisibleTest = { x -> x % divisibleBy == 0 }
+        divisibleBy = monkeyDescription[3].drop("  Test: divisible by ".length).toLong()
+        divisibleTest = { x -> x % divisibleBy == 0L }
         trueTarget = monkeyDescription[4].drop("    If true: throw to monkey ".length).toInt()
         falseTarget = monkeyDescription[5].drop("    If false: throw to monkey ".length).toInt()
     }
 
-    private fun parseOperation(s: String): (Int) -> Int {
+    private fun parseOperation(s: String): (Long) -> Long {
         val (_, operationString, secondOperandString) = s.drop("  Operation: new = ".length)
             .split(" ")
         //assuming first operand is always "old"
         //assuming only + and * operations exist
-        val op: (Int, Int) -> Int =
+        val op: (Long, Long) -> Long =
             if (operationString == "+") { a, b -> a + b } else { a, b -> a * b }
         //second operator can be old or a number
-        val secondOp: (Int) -> Int =
-            if (secondOperandString == "old") { x -> x } else { x -> secondOperandString.toInt() }
+        val secondOp: (Long) -> Long =
+            if (secondOperandString == "old") { x -> x } else { _ -> secondOperandString.toLong() }
         return { old -> op(old, secondOp(old)) }
     }
 
 
-    fun inspectItem(): Pair<Int, Int>? {
+    fun inspectItem(): Pair<Int, Long>? {
         if (holdingItems.isEmpty()) {
             return null
         }
         inspectionCount++
         var itemToInspect = holdingItems.removeFirst()
         itemToInspect = operation(itemToInspect)
-        itemToInspect = itemToInspect / 3 // that was for part 1
+        //itemToInspect = itemToInspect / 3 // that was for part 1
+        itemToInspect %= reduceWorryFactor
         return if (divisibleTest(itemToInspect)) {
             Pair(trueTarget, itemToInspect)
         } else {
@@ -49,7 +51,7 @@ class Monkey(monkeyDescription: List<String>) {
         }
     }
 
-    fun giveItem(newItem: Int) {
+    fun giveItem(newItem: Long) {
         holdingItems.addLast(newItem)
     }
 
--- src/main/kotlin/day11/day11.kt
+++ src/main/kotlin/day11/day11.kt
@@ -1,5 +1,6 @@
 package day11
 
+import org.apache.commons.math3.util.ArithmeticUtils
 import java.nio.file.Files
 import java.nio.file.Path
 
@@ -7,20 +8,26 @@ fun main() {
     val allLines = Files.readAllLines(Path.of("src/main/kotlin/day11/input.txt"))
     val monkeys = allLines.filter { it.isNotBlank() }.chunked(6).map { Monkey(it) }.toList()
 
-    repeat(20) {
+    val lcm = monkeys.map { it.divisibleBy }.reduce { a, b -> ArithmeticUtils.lcm(a, b) }
+
+    monkeys.forEach { it.reduceWorryFactor = lcm }
+    repeat(10_000) {
         monkeys.forEachIndexed { i, monkey ->
             while (true) {
                 val targetAndItemPair = monkey.inspectItem() ?: break
-                //println("throwing item ${targetAndItemPair.second} from monkey $i to ${targetAndItemPair.first}")
+                println(
+                    "throwing item ${targetAndItemPair.second} from " +
+                            "monkey $i to ${targetAndItemPair.first}"
+                )
                 monkeys[targetAndItemPair.first].giveItem(targetAndItemPair.second)
 
             }
         }
     }
-    val monkeyBusiness = monkeys.sortedBy { it.inspectionCount }.reversed().take(2)
-        .map { it.inspectionCount }
-        .reduce { a, b -> a * b }
+    val monkeyBusiness = monkeys.sortedByDescending { it.inspectionCount }.take(2)
+        .map { it.inspectionCount }.reduce { a, b -> a * b }
 
     println(monkeys.map { it.inspectionCount })
-    println("The level of monkey business after 20 rounds: $monkeyBusiness")
+    println("The level of monkey business after 10000 rounds: $monkeyBusiness")
+
 }
